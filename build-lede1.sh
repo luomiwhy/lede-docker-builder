58bf6239d85e96f44f3e84440a78de3c98c2bbf4
luomiwhy <luomiwhy@gmail.com>
2021-03-13 20:11:23 +0800

    Update build-lede1.sh

--- build-lede1.sh
+++ build-lede1.sh
@@ -4,7 +4,7 @@ export ALL_PROXY=http://192.168.137.1:7890/
 
 cd openwrt
 cp -f ../diy-part1.sh . && ./diy-part1.sh
-./scripts/feeds clean
+# ./scripts/feeds clean
 ./scripts/feeds update -a 
 ./scripts/feeds install -a
 cp -f ../diy-part2.sh . &&  ./diy-part2.sh
