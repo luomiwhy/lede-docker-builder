2447d5f271c39d71abddfe8aafea50e604538dd2
luomiwhy <luomiwhy@gmail.com>
2021-03-13 16:15:11 +0800

    Update build-lede1.sh

--- build-lede1.sh
+++ build-lede1.sh
@@ -4,7 +4,6 @@ export ALL_PROXY=http://192.168.137.1:7890/
 
 git clone --depth 1 -b 21.02 --single-branch https://github.com/Lienol/openwrt openwrt
 cd openwrt
-chmod +x *.sh
 cp -f /data/file/diy-part1.sh . && ./diy-part1.sh
 ./scripts/feeds clean
 ./scripts/feeds update -a 
