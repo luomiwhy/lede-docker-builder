306c50659b389dac4da6bb0493547275282fe177
luomiwhy <luomiwhy@gmail.com>
2021-03-13 16:57:55 +0800

    Update build-lede1.sh

--- build-lede1.sh
+++ build-lede1.sh
@@ -2,10 +2,6 @@
 cd 
 export ALL_PROXY=http://192.168.137.1:7890/
 
-git clone --depth 1 -b 21.02 --single-branch https://github.com/Lienol/openwrt openwrt
-
-docker run --rm -it -v /data/lede/dl:/home/build/openwrt/dl -v $(pwd)/:/home/build/ lede
-
 cd openwrt
 cp -f /data/file/diy-part1.sh . && ./diy-part1.sh
 ./scripts/feeds clean
