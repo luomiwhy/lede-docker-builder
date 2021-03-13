4c212b076ae93109046b1db6d86f6ae95564e461
luomiwhy <luomiwhy@gmail.com>
2021-03-13 16:35:05 +0800

    Update build-lede2.sh

--- build-lede2.sh
+++ build-lede2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #Download package
-if [ ! -d "/data/lede/dl" ];then
-    sudo ls dl
-    sudo mkdir -p /data/lede/dl/
+if [ ! -d "/data/lede" ];then
+    sudo ls 
+    sudo mkdir -p /data/lede/
     sudo chown -R ll:ll /data/
 else
     echo "文件夹已经存在"
 fi
 
-mkdir dl && cp -uvr /data/lede/dl dl/
+mkdir dl && cp -uvr /data/lede/ dl/
 make download -j8
-cp -uvr dl/ /data/lede/dl
+cp -uvr dl/ /data/lede/
 
 # First Compile the firmware
 make -j1 V=s
